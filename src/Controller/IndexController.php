b2c6d11ad58a74d27a9c1e9c6a27d3c5c7c78590
Lova RABARISON <lrabarison@myagency.mg>
2022-05-31 16:37:41 +0200

    Creation controller de test

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends BaseController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_login');
+    }
+}
